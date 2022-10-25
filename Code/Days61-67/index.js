1d96c1806e4f68d95c0c956bf1d926918b8a14d4
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2022-10-25 01:04:42 -0500

    Day 77

--- Code/Days61-67/index.js
+++ Code/Days61-67/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 // Create ExpressJS Server
 const express = require('express')
+const { Console } = require('console')
 const app = express()
 
 // Activate EJS view engine
@@ -14,7 +15,46 @@ app.use(express.urlencoded({ extended: true })) // Parse incoming request bodies
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('index')
+  const filePath = path.join(__dirname, 'public', 'data', 'profiles.json')
+
+  try {
+    const fileData = fs.readFileSync(filePath)
+    const existingProfiles = JSON.parse(fileData)
+
+    if (existingProfiles.length === 0) {
+      res.render('index', {
+        existingProfiles: [],
+      })
+    } else {
+      res.render('index', {
+        existingProfiles: existingProfiles,
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    const existingProfiles = []
+
+    res.render('index', {
+      existingProfiles: existingProfiles,
+    })
+  }
+})
+
+app.post('/save-profile', async function (req, res) {
+  const filePath = path.join(__dirname, 'public', 'data', 'profiles.json')
+  const profileInformation = req.body
+
+  const fileData = fs.readFileSync(filePath)
+  const existingProfiles = JSON.parse(fileData)
+  existingProfiles.push(profileInformation)
+  fs.writeFileSync(filePath, JSON.stringify(existingProfiles))
+  res.render('index', {
+    existingProfiles: existingProfiles,
+  })
+})
+
+app.get('/load-profile', async function (req, res) {
+  const filePath = path.join(__dirname, 'public', 'data', 'profiles.json')
 })
 
 app.listen(3000)
