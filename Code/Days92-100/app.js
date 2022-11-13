dd5a0c5ecb5a246ead5411c97c107563d4cad862
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2022-11-13 16:32:44 -0600

    Day 93

---
+++ Code/Days92-100/app.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const express = require('express')
+
+const db = require('./data/database')
+const authRoutes = require('./routes/auth.routes')
+
+const app = express()
+
+// Views
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Public files
+app.use(express.static('public'))
+
+// Routes
+app.use(authRoutes)
+
+db.connectToDatabase()
+  .then(function () {
+    // Port
+    app.listen(3000)
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!')
+    console.log(error)
+  })
---
+++ Code/Days92-100/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+function getSignup(req, res) {
+  res.render('customer/auth/signup')
+}
+
+function signup(req, res) {}
+
+function getLogin(req, res) {
+  res.render('customer/auth/login')
+}
+
+module.exports = {
+  getSignup: getSignup,
+  getLogin: getLogin,
+  signup: signup,
+}
