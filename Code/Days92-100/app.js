475d9f75513d0c7f932b0ef9a6063e230601c94e
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2022-11-14 23:30:51 -0600

    Day 94

--- Code/Days92-100/app.js
+++ Code/Days92-100/app.js
@@ -1,9 +1,17 @@
 const path = require('path')
 
 const express = require('express')
+const csrf = require('csurf')
+const expressSession = require('express-session')
 
+const createSessionConfig = require('./config/session')
 const db = require('./data/database')
+const addCsrfTokenMiddleware = require('./middlewares/crsf-token')
+const errorHandlerMiddleware = require('./middlewares/error-handler')
+const checkAuthStatusMiddleware = require('./middlewares/check-auth')
 const authRoutes = require('./routes/auth.routes')
+const productsRoutes = require('./routes/products.routes')
+const baseRoutes = require('./routes/base.routes')
 
 const app = express()
 
@@ -14,8 +22,27 @@ app.set('views', path.join(__dirname, 'views'))
 // Public files
 app.use(express.static('public'))
 
+// Extract request information
+app.use(express.urlencoded({ extended: false }))
+
+// Sessions
+const sessionConfig = createSessionConfig()
+app.use(expressSession(sessionConfig))
+
+// Chec Authentication after request
+app.use(checkAuthStatusMiddleware)
+
+// CSRF
+app.use(csrf())
+app.use(addCsrfTokenMiddleware)
+
 // Routes
+app.use(baseRoutes)
 app.use(authRoutes)
+app.use(productsRoutes)
+
+// Error handling
+app.use(errorHandlerMiddleware)
 
 db.connectToDatabase()
   .then(function () {
--- Code/Days92-100/controllers/auth.controller.js
+++ Code/Days92-100/controllers/auth.controller.js
@@ -1,15 +1,75 @@
+const User = require('../models/user.model')
+const authUtil = require('../util/authentication')
 function getSignup(req, res) {
   res.render('customer/auth/signup')
 }
 
-function signup(req, res) {}
+async function signup(req, res, next) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city,
+  )
+
+  try {
+    await user.signup()
+  } catch (error) {
+    next(error)
+    return
+  }
+
+  res.redirect('/login')
+}
 
 function getLogin(req, res) {
   res.render('customer/auth/login')
+}
+
+async function login(req, res) {
+  // Find User
+  const user = new User(req.body.email, req.body.password)
+  let existingUser
+  try {
+    existingUser = await user.getUserWithSameEmail()
+  } catch (error) {
+    next(error)
+    return
+  }
+
+  if (!existingUser) {
+    res.redirect('/login')
+    // TODO: keep information
+    return
+  }
+
+  // Match Password
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    existingUser.password,
+  )
+  if (!passwordIsCorrect) {
+    res.redirect('/login')
+    // TODO: keep information
+    return
+  }
+  // Store Session
+  authUtil.createUserSession(req, existingUser, function () {
+    res.redirect('/')
+  })
+}
+
+function logout(req, res) {
+  // Remove Session Data
+  authUtil.destroyUserAuthSession(req)
+  res.redirect('/login')
 }
 
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin,
   signup: signup,
+  login: login,
+  logout: logout,
 }
