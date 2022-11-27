9ac5406e5d19324eae3aca687891f9e97b85d096
Hugo Pérez <47252665+Hugondon@users.noreply.github.com>
2022-11-27 17:46:52 -0600

    Cart Items Functionalities

--- Code/Days92-100/app.js
+++ Code/Days92-100/app.js
@@ -10,10 +10,12 @@ const addCsrfTokenMiddleware = require('./middlewares/crsf-token')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
 const checkAuthStatusMiddleware = require('./middlewares/check-auth')
 const protectRoutesMiddleWare = require('./middlewares/protect-routes')
+const cartMiddleWare = require('./middlewares/cart')
 const authRoutes = require('./routes/auth.routes')
 const productsRoutes = require('./routes/products.routes')
 const baseRoutes = require('./routes/base.routes')
 const adminRoutes = require('./routes/admin.routes')
+const cartRoutes = require('./routes/cart.routes')
 
 const app = express()
 
@@ -29,6 +31,9 @@ app.use('/products/assets', express.static('product-data'))
 // Extract request information
 app.use(express.urlencoded({ extended: false }))
 
+// Check requests for JSON data
+app.use(express.json())
+
 // Sessions
 const sessionConfig = createSessionConfig()
 app.use(expressSession(sessionConfig))
@@ -38,12 +43,17 @@ app.use(checkAuthStatusMiddleware)
 
 // CSRF
 app.use(csrf())
+
+// Cart Middleware
+app.use(cartMiddleWare)
+
 app.use(addCsrfTokenMiddleware)
 
 // Routes
 app.use(baseRoutes)
 app.use(authRoutes)
 app.use(productsRoutes)
+app.use('/cart', cartRoutes)
 
 app.use(protectRoutesMiddleWare)
 // Only paths that start with /admin
---
+++ Code/Days92-100/middlewares/cart.js
@@ -0,0 +1,21 @@
+// Check incoming requests
+const Cart = require('../models/cart.models')
+
+function initializeCart(req, res, next) {
+  let cartItem
+  if (!req.session.cart) {
+    cart = new Cart()
+  } else {
+    const sessionCart = req.session.cart
+    cart = new Cart(
+      sessionCart.items,
+      sessionCart.totalQuantity,
+      sessionCart.totalPrice,
+    )
+  }
+
+  res.locals.cart = cart
+  next()
+}
+
+module.exports = initializeCart
